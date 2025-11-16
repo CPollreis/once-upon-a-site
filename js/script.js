ce5ff691531ee5ca3f0c9342d1d2b2cae94b1cd0
ethaneholland <70114122+ethaneholland@users.noreply.github.com>
2025-11-16 13:43:36 -0600

    Updating the new UI design with JS

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 const MENU_STATE = 0;
 const CLUB_PAGE_STATE = 1;
 // Menu page locations. Change these to point to the first page of each section.
+const PAGE_MAPPING = ["home", "toc", "saved-clubs", "technology", "hobbies", "cultural", "category-4", "category-5", "category-6", "search"];
 const HOME_START = 1;
 const TOC_START = 2;
 const SAVED_CLUBS_START = 3;
@@ -13,17 +14,18 @@ const SEARCH_START = 7;
 
 /******* GLOBAL STATE VARIABLES *******/
 var activeTab; // Currently active tab, active tab is raised. String of current section ID.
-var book_state; // Equal to MENU_STATE or CLUB_PAGE_STATE
+var bookState; // Equal to MENU_STATE or CLUB_PAGE_STATE
+var savedCLubsList = [];
 
 
 /******* INITIALIZATION *******/
 window.addEventListener('load', function () {
-    console.log("Webpage has loaded");
+    console.log("The webpage has loaded, start initialization.")
     
     $("#book-frame").turn({when: {turn: turnEvent}, display: "single",}); // Initialize turn.js
     
     initMenuPages(); // Add the menu pages
-    book_state = MENU_STATE;
+    bookState = MENU_STATE;
     
     // Activate the home tab
     activeTab = null;
@@ -39,14 +41,14 @@ window.addEventListener('load', function () {
 // Call this to set the book to the menu pages.
 // You will need to flip to the right page yourself.
 function initMenuPages() {
-    book_state = MENU_STATE;
-    removedAPage = removeAllButOnePage();
+    bookState = MENU_STATE;
+    onePageRemaining = removeAllButOnePage();
     var pages = document.getElementById("menu-pages");
     for (const child of pages.children) {
         $("#book-frame").turn("addPage", child.cloneNode(true));
         console.log("Adding page: " + $("#book-frame").turn("pages"));
     }
-    if (removedAPage) {
+    if (onePageRemaining) {
         $("#book-frame").turn("removePage", 1);
     }
     console.log("Finished adding menu pages")
@@ -57,14 +59,14 @@ function initMenuPages() {
 // You will need to flip to the right page yourself.
 function initClubPages(clubPageList) {
     console.log("initializing club pages");
-    book_state = CLUB_PAGE_STATE;
-    removedAPage = removeAllButOnePage();
+    bookState = CLUB_PAGE_STATE;
+    onePageRemaining = removeAllButOnePage();
     console.log(clubPageList);
     for (const child of clubPageList) {
         $("#book-frame").turn("addPage", child.cloneNode(true));
         console.log("Adding page: " + $("#book-frame").turn("pages"));
     }
-    if (removedAPage) {
+    if (onePageRemaining) {
         $("#book-frame").turn("removePage", 1);
     }
     console.log("Finished adding club pages");
@@ -75,14 +77,17 @@ function initClubPages(clubPageList) {
 // This will remove all but one page.
 // You will need to remove the remaining page yourself after adding new pages, so that the book isn't left empty.
 function removeAllButOnePage() {
-    removedAPage = false;
+    onePageRemaining = false;
     $("#book-frame").turn("page", 1);
     while ($("#book-frame").turn("pages") > 1) {
         $("#book-frame").turn("removePage", 1);
-        removedAPage = true;
+        onePageRemaining = true;
+    }
+    if ($("#book-frame").turn("pages") === 1) {
+        onePageRemaining = true;
     }
     console.log("removed club pages");
-    return removedAPage;
+    return onePageRemaining;
 }
 
 // activateListeners()
@@ -99,30 +104,16 @@ function activateListeners() {
 function turnEvent() {
     var pages = $('#book-frame').turn('view'); 
 
-    if (book_state === CLUB_PAGE_STATE) {
+    if (bookState === CLUB_PAGE_STATE) {
         // Might do something here later
     }
-    // If we are in the MENU_STATE, sequentially compare the active page to the next section until we've a match.
-    else if (pages[0] < TOC_START) {
-        activateTab("home");
-    }
-    else if (pages[0] < SAVED_CLUBS_START) {
-        activateTab("toc");
-    }
-    else if (pages[0] < TECHNOLOGY_START) {
-        activateTab("saved-clubs");
-    }
-    else if (pages[0] < HOBBIES_START) {
-        activateTab("technology");
-    }
-    else if (pages[0] < CULTURAL_START) {
-        activateTab("hobbies");
-    }
-    else if (pages[0] < SEARCH_START) {
-        activateTab("cultural");
-    }
     else {
-        activateTab("search");
+        if (pages[0] < PAGE_MAPPING.length) {
+            activateTab(PAGE_MAPPING[ pages[0] -1 ]);
+        }
+        else {
+            activateTab(PAGE_MAPPING[ PAGE_MAPPING.length - 1 ]);
+        }
     }
 }
 
@@ -160,68 +151,117 @@ function clickEvent(event) {
     console.log("click event triggered");
     console.log(event.target.classList[1]);
     if (event.target.classList[1] === "tab") {
-        tabClick(event.target.classList[2]);
+        tabClick(event.target.classList[3]);
     }
     else if (event.target.classList[1] === "club-button") {
         console.log(event.target.getElementsByClassName("club-name")[0].textContent);
-        clubButtonClick(event.target.getElementsByClassName("club-name")[0].textContent);
+        clubButtonClick(event);
+    }
+    else if (event.target.classList[1] === "save-button") {
+        saveButtonClick(event);
     }
 }
 
 // tabClick(id)
 // Pass the tab name (e.g. "home", "saved-clubs")
 // If we are in the MENU_STATE, then jump to the new page and 
-function tabClick(tab_name) {
+function tabClick(tabName) {
     console.log("starting tabClick");
-    pageDestination = 1;
-    switch (tab_name) {
-        case "home":
-            console.log("home");
-            pageDestination = HOME_START;
-            break;
-        case "toc":
-            pageDestination = TOC_START;
-            break;
-        case "saved-clubs":
-            pageDestination = SAVED_CLUBS_START;
-            break;
-        case "technology":
-            pageDestination = TECHNOLOGY_START;
-            break;
-        case "hobbies":
-            pageDestination = HOBBIES_START;
-            break;
-        case "cultural":
-            pageDestination = CULTURAL_START;
-            break;
-        case "search":
-            pageDestination = SEARCH_START;
-            break;
-    }
-    if (book_state === CLUB_PAGE_STATE) {
+    pageDestination = PAGE_MAPPING.indexOf(tabName);
+    if (bookState === CLUB_PAGE_STATE) {
         initMenuPages();
     }
-    $("#book-frame").turn("page", pageDestination);
+    $("#book-frame").turn("page", pageDestination+1);
 }
 
 // clubButtonClick(clubName)
 // clubName: String title of the club (e.g. ".devClub", "The French Club")
 // A club button has been clicked.
-function clubButtonClick(clubName) {
+function saveButtonClick(event) {
+    var sticker;
+    var stickerList;
+    var page;
+    var clubName = event.target.parentElement.getElementsByClassName("club-name")[0].textContent;
+    var saved = event.target.parentElement.getElementsByClassName("club-name")[0].classList.contains("saved");
+    var activeClubPage = event.target.closest(".club-page");
+    var saved = activeClubPage.getElementsByClassName("save-button")[0].classList.contains("saved");
+    var clubPageTemplate;
+    for (page of document.getElementById("club-page-list").children) {
+        if (page.getElementsByClassName("club-name")[0].textContent === clubName) {
+            clubPageTemplate = page;
+            break;
+        }
+    }
+
+    if (saved) {
+        stickerList = document.getElementById("saved-clubs-page").getElementsByClassName("club-button");
+        for (sticker of stickerList) {
+            if (sticker.getElementsByClassName("club-name")[0].textContent === clubName) {
+                sticker.parentNode.removeChild(sticker);
+                break;
+            }
+        }
+        activeClubPage.getElementsByClassName("save-button")[0].classList.remove("saved");
+        clubPageTemplate.getElementsByClassName("save-button")[0].classList.remove("saved");
+        event.target.textContent = "Save Club";
+        clubPageTemplate.getElementsByClassName("save-button")[0].textContent = "Save Club";
+    }
+    else {
+        stickerList = document.getElementById("club-stickers-list").getElementsByClassName("club-button");
+        for (sticker of stickerList) {
+            console.log(sticker);
+            if (sticker.getElementsByClassName("club-name")[0].textContent === clubName) {
+                addClubSticker( document.getElementById("saved-clubs-page"), sticker.cloneNode(true) );
+                activeClubPage.getElementsByClassName("save-button")[0].classList.add("saved");
+                clubPageTemplate.getElementsByClassName("save-button")[0].classList.add("saved");
+                activeClubPage.getElementsByClassName("save-button")[0].textContent = "Saved";
+                clubPageTemplate.getElementsByClassName("save-button")[0].textContent = "Saved";
+                break;
+            }
+        }
+    }
+}
+
+// addClubSticker(page, sticker)
+// page: element of the page that we will add the sticker to
+// sticker: element of the sticker that we will add
+// returns true if a sticker was added successfully
+function addClubSticker(page, sticker) {
+    var numberOfStickers = page.getElementsByClassName("club-button").length;
+    if (numberOfStickers < 4) {
+        page.getElementsByClassName("half-page")[0].appendChild(sticker);
+    }
+    else if (numberOfStickers < 8) {
+        page.getElementsByClassName("half-page")[1].appendChild(sticker);
+    }
+}
+
+// clubButtonClick(clubName)
+// clubName: String title of the club (e.g. ".devClub", "The French Club")
+// A club button has been clicked.
+function clubButtonClick(event) {
     var clubPageList;
+    var clubName = event.target.getElementsByClassName("club-name")[0].textContent
     // The active tab will tell us where the club button was pressed.
     if (activeTab === "search") {
         // TBD: Generate a list of pages corresponding to the search filters.
     }
     else if (activeTab === "toc") {
-        clubPageList = getClubPagesByAlpha();
+        clubPageList = document.getElementById("club-page-list").children;
     }
     else if (activeTab === "saved-clubs") {
-        // TBD: Generate a list of saved clubs.
+        clubPageList = [];
+        for (sticker of document.getElementById("saved-clubs-page").getElementsByClassName("club-button")) {
+            for (club of document.getElementById("club-page-list").children) {
+                if (club.getElementsByClassName("club-name")[0].textContent === sticker.getElementsByClassName("club-name")[0].textContent) {
+                    clubPageList.push(club);
+                }
+            }
+        }
     }
     else {
         // Generate a list of clubs corresponding to the current category.
-        clubPageList = getClubPagesByCategory(activeTab);
+        clubPageList = document.getElementById("club-page-list").getElementsByClassName(activeTab);
     }
 
     // Remove the menu pages and add the list of club pages to the book.
@@ -234,21 +274,4 @@ function clubButtonClick(clubName) {
             break;
         }
     }
-}
-
-// getClubPagesByCategory(category)
-// category: String id of the club page category (e.g. "technology", "hobbies")
-// Return: An array of club pages that match the provided category
-function getClubPagesByCategory(category) {
-    // Get the div containing all club pages. 
-    // Then generate and return a list of any div that contains the "category" as an id.
-    return document.getElementById("club-pages").getElementsByClassName(category);
-}
-
-// getClubPagesByAlpha()
-// Return: An array of club pages in alphabetical order
-function getClubPagesByAlpha() {
-    // Get the div containing all club pages. 
-    // This current implementation relies on the club pages already sorted in alphabetical order.
-    return document.getElementById("club-pages").children;
 }
